7afeed6747f58e8e443981690068b53c8db015ff
yulia <yuliagorbunova@yahoo.com>
2023-01-21 00:14:30 +0500

    ex_4

---
+++ ex_4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+принимает число проверяет ли одновременно кратно оно 7 и 23
+*/
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 7 ==0&&number % 23 ==0)
+    {
+        Console.WriteLine("Число кратно");
+    }
+else
+{
+    Console.WriteLine("Число не кратно");
+}
+
