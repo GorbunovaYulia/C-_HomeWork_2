82c6f21d64f83433844d313b32bedfb60d02c40f
yulia <yuliagorbunova@yahoo.com>
2023-01-21 01:12:30 +0500

    HW_2

--- hw_2/Program.cs
+++ hw_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+float PositiveNumber = MathF.Sqrt(number*number);
+if (PositiveNumber<100)
+{
+    Console.WriteLine("третьего числа нет");
+}
+else 
+{
+    Console.WriteLine();
+}
