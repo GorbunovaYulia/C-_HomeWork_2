b2772d5226b8ccf93fcbd83692be8dd2108f495a
yulia <yuliagorbunova@yahoo.com>
2023-01-21 00:19:55 +0500

    ex_5

---
+++ ex_5/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+принимает 2 числа, проверяет является ли одно число квадратом другого
+*/
+Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1==number2*number2||number2==number1*number1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+     Console.WriteLine("Нет");
+}
